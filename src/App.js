e6faba36fa08a63e934831a296ced69ffbfdf28a
Caleb Dauphin <calebdauphindev@gmail.com>
2022-04-10 15:26:37 -0400

    add react and clean up app.js

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { Component } from "react"
+import "./App.css"
+
+class App extends Component {
+	constructor() {
+		super()
+
+		this.state = {
+			monsters: []
+		}
+		console.log("constructor")
+	}
+
+	componentDidMount() {
+		console.log("componentDidMount")
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then(response => response.json())
+			.then(users =>
+				this.setState(
+					() => {
+						return { monsters: users }
+					},
+					() => {
+						console.log(this.state)
+					}
+				)
+			)
+	}
+
+	render() {
+		console.log("render")
+		return (
+			<div className='App'>
+				{this.state.monsters.map(monster => {
+					return (
+						<div key={monster.id}>
+							<h1>{monster.name}</h1>
+						</div>
+					)
+				})}
+			</div>
+		)
+	}
+}
+
+export default App
